a3fd257c411ba8966c6973d1520af57d31833e6d
Austin Baltes <austinba@gmail.com>
2017-03-05 13:14:17 -0500

    passes test for adding user

--- src/actions/tests/usersTest.js
+++ src/actions/tests/usersTest.js
@@ -2,13 +2,30 @@ const currentUser = '00000000-0000-0000-0000-000000000101';
 process.env.NODE_ENV = 'dev';
 
 // 'import' hoists so use 'require' so we can set NODE_ENV first.
-const { getAllUsers, getUsersFromIDs } = require('../users');
-const testUsers = () => Promise.all([
-  getAllUsers(),
-  getUsersFromIDs([
-    '00000000-0000-0000-0000-000000000101',
-    '00000000-0000-0000-0000-000000000103'
+const userActions = require('../users');
+
+
+// test retrieval of users
+const test1 = () => Promise.all([
+  userActions.getAllUsers(),
+  userActions.getUsersFromIDs([
+    'austin@qs',
+    'chris@qs'
   ])
 ]).then(console.log).catch(console.log.bind(null, 'error'));
 
-testUsers();
+// test1();
+
+
+// can create a user
+const test2 = () =>
+  userActions.addUser({
+    firstName: 'austin',
+    lastName: 'baltes',
+    password: 'the-best',
+    teamdomain: 'qs',
+    username: 'a8bman',
+    email: 'aus@ba.com'})
+    .then(console.log).catch(console.log.bind(null, 'error'));
+
+test2();
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,22 +1,30 @@
 import R from 'ramda';
 import Promise from 'bluebird';
+import bcrypt from 'bcryptjs'
 import normalizeEmail from 'normalize-email';
 import { postProcessScan, postProcessGetItems, postProcessGetItem } from './utilities';
 import { User } from '../model';
 import config from '../app_config.json';
 const JWTSecret = Buffer.from(config.jwt.secret, 'hex');
 
+//
+// SECURITY NOTE: Only pass on password & password hash if absolutely necessary
+// We don't want this to show up in a user response
+//
+
 /** Replaces User fields with normalized versions of the same parameters */
 const normalize =
-R.evolve({
-  username:   R.toLower,
-  teamdomain: R.toLower,
-  email:      R.apply(normalizeEmail)
-});
+  R.evolve({
+    username:   R.toLower,
+    teamdomain: R.toLower,
+    email:      email => normalizeEmail(email)
+  });
 const keys = R.pick(['username', 'teamdomain']);
+const fields = R.pick(['username', 'teamdomain', 'email', 'firstName', 'lastName', 'password']);
 const keysNormalized = R.pipe(keys, normalize);
+const fieldsNormalized = R.pipe(fields, normalize);
 const addUserID = user => R.assoc('userID', joinUserID(user), user);
-const dropPasswordHash = R.dissoc('passwordHash');
+const dropPasswordAndHash = R.pipe(R.dissoc('passwordHash'), R.dissoc('password'));
 
 
 export function getAllUsers() {
@@ -26,8 +34,7 @@ export function getAllUsers() {
       R.map(R.pipe(
         addUserID,
         dropPasswordHash
-      )),
-      t=>console.log(t) ||t
+      ))
     )) // add the compound userID
 }
 /** username@teamdomain => {username, teamdomain}*/
@@ -43,12 +50,17 @@ export function joinUserID({username, teamdomain}) {
 }
 // this is going to go away -- but needed for mock data
 export function getUsersFromIDs(ids) {
-  return User.getItemsAsync(ids).then(postProcessGetItems);
+  return User.getItemsAsync(R.map(splitUserID)(ids))
+    .then(postProcessGetItems);
 }
 export function addUser(fields) {
-  return hashPassword(normalize(fields))
+  return hashPassword(fieldsNormalized(fields))
+    .then(R.omit('password')) // don't send to db, now that it is converted to a hash
     .then(fields => User.createAsync(fields, { overwrite: false }))
-    .then(postProcessGetItem);
+    .then(R.pipe(
+        postProcessGetItem,
+        dropPasswordAndHash
+      ));
 }
 export function getUser(fields) {
   return User.getItemAsync(keysNormalized(fields))
